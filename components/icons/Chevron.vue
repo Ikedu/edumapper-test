d85bcbd4f1830c975b6f3bc4841b1d1830791354
Ikedu <robainerafik@gmail.com>
2025-06-08 18:30:22 +0200

    feat(ui): add icons

---
+++ components/icons/Chevron.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  isUp?: boolean
+}>()
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="transition-transform duration-300"
+    :class="{ 'rotate-180': isUp }"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <polyline points="6 9 12 15 18 9" />
+  </svg>
+</template>
---
+++ components/icons/Pencil.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M2 14L8 14L14 14"
+      stroke="#1B1B1B"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.14805 3.88559L10.0337 1.99997L13.3335 5.2998L11.4479 7.18542M8.14805 3.88559L4.50794 7.52569C4.32041 7.71323 4.21505 7.96758 4.21505 8.2328L4.21505 11.1184L7.10067 11.1184C7.36588 11.1184 7.62024 11.0131 7.80778 10.8255L11.4479 7.18542M8.14805 3.88559L11.4479 7.18542"
+      stroke="#1B1B1B"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
