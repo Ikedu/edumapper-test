6dced942f377ff53f2647b4bf8f66db024e3479f
Ikedu <robainerafik@gmail.com>
2025-06-09 17:48:17 +0200

    feat(ui): add trust stars component

---
+++ components/ui/TrustStars.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  confidenceIndex: number
+  maxStars?: number
+}>(), {
+  maxStars: 5,
+})
+
+const stars = computed(() =>
+    Array.from({length: props.maxStars}, (_, i) => i < props.confidenceIndex)
+)
+</script>
+
+<template>
+  <div class="flex items-center gap-1 text-lg">
+    <Star
+        v-for="(active, i) in stars"
+        :key="i"
+        :class="{ 'opacity-30': !active }"
+        class="h-3 w-3 text-[#3890FA]"
+    />
+  </div>
+</template>
